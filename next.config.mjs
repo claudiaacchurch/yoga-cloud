ff266c08ccf4981585cf71125abc3851162aad32
claudiaacchurch <claudiachurch00@gmail.com>
2025-08-08 21:53:50 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',          // enables `next export`
-    images: { unoptimized: true }, // required for GH Pages
-    trailingSlash: true,       // nicer static URLs (optional)
-  
-    // ONLY if this is a *project page* (repo like user/yoga-cloud)
+    output: 'export',
+    images: { unoptimized: true },
     basePath: '/yoga-cloud',
     assetPrefix: '/yoga-cloud/',
+    trailingSlash: true
   };
+  
   export default nextConfig;
   
